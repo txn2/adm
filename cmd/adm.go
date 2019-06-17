c89c6f3d9f6857d37d609ca6a2932e533a4a9110
Craig Johnston <cjimti@gmail.com>
2019-06-17 14:33:01 -0700

    added token check

--- cmd/adm.go
+++ cmd/adm.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"flag"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
 	"github.com/txn2/ack"
 	"github.com/txn2/micro"
 	"github.com/txn2/provision"
+	"go.uber.org/zap"
 )
 
 var (
@@ -26,9 +33,103 @@ func main() {
 	// User token middleware
 	server.Router.Use(provision.UserTokenHandler())
 
+	// cache
+	csh := cache.New(1*time.Minute, 10*time.Minute)
+	provisionService := *provisionScheme + "://" + *provisionHost
+
+	// check account against access key
+	checkAccount := func(accountId string, accessKey provision.AccessKey) (bool, error) {
+
+		cacheKey := accountId + accessKey.Name + accessKey.Key
+
+		// check cache
+		cacheResult, found := csh.Get(cacheKey)
+		if found {
+			return cacheResult.(bool), nil
+		}
+
+		url := provisionService + "/keyCheck/" + accountId
+
+		accountKeyJson, err := json.Marshal(accessKey)
+		if err != nil {
+			csh.Set(cacheKey, false, cache.DefaultExpiration)
+			return false, err
+		}
+		req, _ := http.NewRequest("POST", url, bytes.NewReader(accountKeyJson))
+		res, err := server.Client.Http.Do(req)
+		if err != nil {
+			server.Logger.Warn(
+				"Provision service request failure.",
+				zap.String("url", url),
+				zap.Error(err))
+
+			csh.Set(cacheKey, false, cache.DefaultExpiration)
+			return false, err
+		}
+
+		if res.StatusCode == 404 {
+			csh.Set(cacheKey, false, cache.DefaultExpiration)
+			return false, errors.New(accountId + " account not found.")
+		}
+
+		if res.StatusCode == 200 {
+			csh.Set(cacheKey, true, cache.DefaultExpiration)
+			return true, nil
+		}
+
+		csh.Set(cacheKey, false, cache.DefaultExpiration)
+		return false, errors.New("got code " + string(res.StatusCode) + " from " + url)
+	}
+
 	accessHandler := func(c *gin.Context) {
 		ak := ack.Gin(c)
 
+		// requests for whoami account return the header value of
+		// X-DCP-Account
+		pa := c.Param("parentAccount")
+		if pa == "whoami" {
+			hdr := c.GetHeader("X-DCP-Account")
+			if hdr == "" {
+				ak.SetPayloadType("ErrorMessage")
+				ak.SetPayload("No X-DCP-Account header.")
+				ak.GinErrorAbort(401, "E401", "X-DCP-Account header was not sent to whoami")
+				return
+			}
+
+			ak.SetPayloadType("WhoAmI")
+			ak.SetPayload(hdr)
+			c.Done()
+			return
+		}
+
+		// Check api key for parentAccount if one exists
+		name, key, ok := c.Request.BasicAuth()
+		if ok {
+			accessKey := provision.AccessKey{
+				Name: name,
+				Key:  key,
+			}
+
+			ok, err := checkAccount(pa, accessKey)
+			if err != nil {
+				ak.SetPayloadType("ErrorMessage")
+				ak.SetPayload("APIKeyCheckError")
+				ak.GinErrorAbort(401, "E401", err.Error())
+				return
+			}
+
+			if !ok {
+				ak.SetPayloadType("ErrorMessage")
+				ak.SetPayload("APIKeyCheckError")
+				ak.GinErrorAbort(401, "E401", "Invalid API Key")
+				return
+			}
+
+			// valid key
+			return
+		}
+
+		// Check token if one exists
 		userI, ok := c.Get("User")
 		if !ok {
 			ak.SetPayloadType("ErrorMessage")
@@ -39,9 +140,12 @@ func main() {
 
 		user := userI.(*provision.User)
 
+		// let sysops though
 		if user.Active && user.Sysop {
 			return
 		}
+
+		// @TODO Check token for parentAccount
 
 		ak.SetPayloadType("ErrorMessage")
 		ak.SetPayload("insufficient privileges")
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/txn2/adm
 
 go 1.12
 
-require github.com/txn2/provision v0.2.0 // indirect
+require (
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/txn2/provision v0.2.0 // indirect
+)
